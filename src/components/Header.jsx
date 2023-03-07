400b3c0347aec1155fb1de1b5793f1a9a29e302b
S-DeMaio <demaio89@gmail.com>
2023-03-07 14:59:53 -0600

    finalizing mailto: links in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -78,9 +78,9 @@ function MobileNavigation() {
             as="div"
             className="absolute inset-x-0 top-full mt-4 flex origin-top flex-col rounded-2xl bg-white p-4 text-lg tracking-tight text-slate-900 shadow-xl ring-1 ring-slate-900/5 z-50"
           >
+            <MobileNavLink href="mailto:sibleym@subtleav.com">Contact</MobileNavLink>
             <MobileNavLink href="#features">Gallery</MobileNavLink>
             <MobileNavLink href="#testimonials">About</MobileNavLink>
-            <MobileNavLink href="mailto:sibleym@subtleav.com">Contact</MobileNavLink>
             <hr className="m-2 border-slate-300/40" />
             {/* <MobileNavLink href="/login">Sign in</MobileNavLink> */}
           </Popover.Panel>
@@ -100,9 +100,9 @@ export function Header() {
               <SvgSubtleAvLogo className="h-auto w-auto" />
             </Link>
             <div className="hidden md:flex md:gap-x-6">
-              <NavLink href="#gallery">Gallery</NavLink>
+              <NavLink href="mailto:sibleym@subtleav.com">Contact</NavLink>
+              <NavLink href="#features">Gallery</NavLink>
               <NavLink href="#about">About</NavLink>
-              <NavLink href="/quote">Contact</NavLink>
             </div>
           </div>
           <div className="flex items-center gap-x-5 md:gap-x-8">
